8fefd19a2928dbf0bb4a6271dd1cf229b7db14a7
Pilunte <vad23@msn.com>
2017-02-01 19:04:10 +0100

    test

--- app_sources/RetroAchievements/controller.js
+++ app_sources/RetroAchievements/controller.js
@@ -1,14 +1,11 @@
 'use strict';
-
+app.controller('RetroAchievementsCtrl', ['$scope','retroachievements','settings', 
+  function($scope, retroachievements,settings) { 
+    
       var userRA =$scope.userRA = settings.$obj.userRA;
       var apiKeyRA = settings.$obj.apiKeyRA;
       $scope.baseUrl = "http://retroachievements.org/";
-
-      retroachievements.GetUserSummary(userRA,apiKeyRA, function(response) {
-          $scope.GetUserSummary = response.data;
-      });
-     
-      
+    
       retroachievements.GetUserSummary(userRA,apiKeyRA,5, function(userdata) {
          
          $scope.GetUserSummary = userdata.data;
