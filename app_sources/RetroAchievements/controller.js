3d5c1514460b6bf5172c094ae3788d9d9c184abb
Pilunte <vad23@msn.com>
2017-02-01 18:47:17 +0100

    game progress

--- app_sources/RetroAchievements/controller.js
+++ app_sources/RetroAchievements/controller.js
@@ -1,8 +1,5 @@
 'use strict';
 
-app.controller('RetroAchievementsCtrl', ['$scope','retroachievements','settings',
-  function($scope, retroachievements,settings) {
-
       var userRA =$scope.userRA = settings.$obj.userRA;
       var apiKeyRA = settings.$obj.apiKeyRA;
       $scope.baseUrl = "http://retroachievements.org/";
@@ -12,6 +9,23 @@ app.controller('RetroAchievementsCtrl', ['$scope','retroachievements','settings'
       });
      
       
+      retroachievements.GetUserSummary(userRA,apiKeyRA,5, function(userdata) {
+         
+         $scope.GetUserSummary = userdata.data;
+         
+         var gameList= "";
+  
+         angular.forEach(userdata.data.RecentlyPlayed, function(value, key) {
+                gameList= value.GameID + "," + gameList ;
+         });
+
+         var gameListClean = gameList.substring(0, gameList.length-1);
+         
+         retroachievements.GetUserProgress(userRA,apiKeyRA,gameListClean, function(progressdata) {
+           $scope.GetUserProgress =progressdata.data
+         });
+    
+      });
 
 
   }
