c6066fa01984702488c75ace7ebd221b01b77624
Pilunte <vad23@msn.com>
2017-02-05 10:52:03 +0100

    Add form progress bar and progress bar point

--- app_sources/RetroAchievements/controller.js
+++ app_sources/RetroAchievements/controller.js
@@ -1,37 +1,38 @@
 'use strict';
-app.controller('RetroAchievementsCtrl', ['$scope','retroachievements','settings', '_',
-  function($scope, retroachievements,settings,_) { 
-    
-      var userRA =$scope.userRA = settings.$obj.userRA;
-      var apiKeyRA = settings.$obj.apiKeyRA;
-      $scope.baseUrl = "http://retroachievements.org/";
-    
-      retroachievements.GetUserSummary(userRA,apiKeyRA,5, function(userdata) {
-         
-         $scope.GetUserSummary = userdata.data;
-         
-         var gameList= "";
-  
-         angular.forEach(userdata.data.RecentlyPlayed, function(value, key) {
-                gameList= value.GameID + "," + gameList ;
-         });
+app.controller('RetroAchievementsCtrl', ['$scope', 'retroachievements', 'settings', '_',
+  function ($scope, retroachievements, settings, _ ) {
 
-         var gameListClean = gameList.substring(0, gameList.length-1);
-         var gameStat =[];
-         retroachievements.GetUserProgress(userRA,apiKeyRA,gameListClean, function(progressdata) {
-           $scope.GetUserProgress =progressdata.data
-           _.forEach(userdata.data.RecentlyPlayed, function(value, key) {
+    var userRA = $scope.userRA = settings.$obj.userRA;
+    var apiKeyRA = settings.$obj.apiKeyRA;
+    $scope.baseUrl = "http://retroachievements.org/";
 
-              _.forEach(progressdata.data, function(value2, key2) {
-                if (value.GameID == key2){
-                   gameStat.push(_.merge(value2, value));             
-                }
-              });
-         });
-         });
-         $scope.gameStat=gameStat;
-         console.log(userdata.data);
+    retroachievements.GetUserSummary(userRA, apiKeyRA, 5, function (userdata) {
+
+      $scope.GetUserSummary = userdata.data;
+
+      var gameList = "";
+
+      angular.forEach(userdata.data.RecentlyPlayed, function (value, key) {
+        gameList = value.GameID + "," + gameList;
       });
+
+      var gameListClean = gameList.substring(0, gameList.length - 1);
+      var gameStat = [];
+      retroachievements.GetUserProgress(userRA, apiKeyRA, gameListClean, function (progressdata) {
+        $scope.GetUserProgress = progressdata.data
+        _.forEach(userdata.data.RecentlyPlayed, function (value, key) {
+
+          _.forEach(progressdata.data, function (value2, key2) {
+            if (value.GameID == key2) {
+              gameStat.push(_.merge(value2, value));
+            }
+          });
+        });
+      });
+      $scope.gameStat = gameStat;
+      console.log(gameStat);
+    });
+
 
   }
 ]);
--- app_sources/RetroAchievements/service.js
+++ app_sources/RetroAchievements/service.js
@@ -1,60 +1,82 @@
 'use strict';
 
 app.factory('retroachievements', ['$http',
-  function ($http) {
-      console.log('retroachievements - init');
-      var retroachievements = {};
-      var baseURL = 'http://retroachievements.org/API/';
+    function ($http) {
+        console.log('retroachievements - init');
+        var retroachievements = {};
+        var baseURL = 'http://retroachievements.org/API/';
 
-      
-      // General Information
-      retroachievements.GetTopTenUsers = function(user,apiKey,callback){
-         $http.get(baseURL + 'API_GetTopTenUsers.php?z='+user+'&y='+apiKey).then(function(response){callback(response)});     
-      }; 
 
-      retroachievements.GetConsoleIDs= function(user,apiKey,callback){
-         $http.get(baseURL + 'API_GetConsoleIDs.php?z='+user+'&y='+apiKey).then(function(response){callback(response)});     
-      };   
+        // General Information
+        retroachievements.GetTopTenUsers = function (user, apiKey, callback) {
+            $http.get(baseURL + 'API_GetTopTenUsers.php?z=' + user + '&y=' + apiKey).then(function (response) {
+                callback(response)
+            });
+        };
 
-      //Game Information
-      retroachievements.GetGameInfo = function(user,apiKey,gameID,callback){
-         $http.get(baseURL + 'API_GetGame.php?z='+user+'&y='+apiKey+'&i='+gameID).then(function(response){callback(response)});     
-      }; 
+        retroachievements.GetConsoleIDs = function (user, apiKey, callback) {
+            $http.get(baseURL + 'API_GetConsoleIDs.php?z=' + user + '&y=' + apiKey).then(function (response) {
+                callback(response)
+            });
+        };
 
-      retroachievements.GetGameInfoExtended= function(user,apiKey,gameID,callback){
-         $http.get(baseURL + 'API_GetGameExtended.php?z='+user+'&y='+apiKey+'&i='+gameID).then(function(response){callback(response)});     
-      };    
-    
-      retroachievements.GetGameInfoAndUserProgress = function(user,apiKey,gameID,callback){
-         $http.get(baseURL + 'API_GetGameInfoAndUserProgress.php?z='+user+'&y='+apiKey+'&u='+user+'&g='+gameID).then(function(response){callback(response)});     
-      };
-      
-      //broken API
-      retroachievements.GetGameList= function(user,apiKey,consoleID,callback){
-         $http.get(baseURL + 'API_GetGameList.php?z='+user+'&y='+apiKey+'&i='+consoleID).then(function(response){callback(response)});     
-      }; 
+        //Game Information
+        retroachievements.GetGameInfo = function (user, apiKey, gameID, callback) {
+            $http.get(baseURL + 'API_GetGame.php?z=' + user + '&y=' + apiKey + '&i=' + gameID).then(function (response) {
+                callback(response)
+            });
+        };
 
-      //User Information
-      retroachievements.GetUserSummary = function(user,apiKey,numRecentGames,callback){
-         $http.get(baseURL + 'API_GetUserSummary.php?z='+user+'&y='+apiKey+'&u='+user+'&g='+numRecentGames).then(function(response){callback(response)});     
-      };
-      retroachievements.GetUserRankAndScore = function(user,apiKey,callback){
-         $http.get(baseURL + 'API_GetUserRankAndScore.php?z='+user+'&y='+apiKey+'&u='+user).then(function(response){callback(response)});     
-      };
+        retroachievements.GetGameInfoExtended = function (user, apiKey, gameID, callback) {
+            $http.get(baseURL + 'API_GetGameExtended.php?z=' + user + '&y=' + apiKey + '&i=' + gameID).then(function (response) {
+                callback(response)
+            });
+        };
 
-      retroachievements.GetUserRecentlyPlayedGames= function(user,apiKey,count,offset,callback){
-         $http.get(baseURL + 'API_GetUserRecentlyPlayedGames.php?z='+user+'&y='+apiKey+'&c='+count+'&o='+offset+'&u='+user).then(function(response){callback(response)});     
-      };
+        retroachievements.GetGameInfoAndUserProgress = function (user, apiKey, gameID, callback) {
+            $http.get(baseURL + 'API_GetGameInfoAndUserProgress.php?z=' + user + '&y=' + apiKey + '&u=' + user + '&g=' + gameID).then(function (response) {
+                callback(response)
+            });
+        };
 
-      retroachievements.GetFeedFor= function(user,apiKey,count,offset,callback){
-         $http.get(baseURL + 'API_GetFeedFor.php?z='+user+'&y='+apiKey+'&c='+count+'&o='+offset+'&u='+user).then(function(response){callback(response)});     
-      };
+        //broken API
+        retroachievements.GetGameList = function (user, apiKey, consoleID, callback) {
+            $http.get(baseURL + 'API_GetGameList.php?z=' + user + '&y=' + apiKey + '&i=' + consoleID).then(function (response) {
+                callback(response)
+            });
+        };
 
-     retroachievements.GetUserProgress = function(user,apiKey,gameList,callback){
-         $http.get(baseURL + 'API_GetUserProgress.php?z='+user+'&y='+apiKey+'&u='+user+"&i="+gameList).then(function(response){callback(response)});     
-      };
+        //User Information
+        retroachievements.GetUserSummary = function (user, apiKey, numRecentGames, callback) {
+            $http.get(baseURL + 'API_GetUserSummary.php?z=' + user + '&y=' + apiKey + '&u=' + user + '&g=' + numRecentGames).then(function (response) {
+                callback(response)
+            });
+        };
+        retroachievements.GetUserRankAndScore = function (user, apiKey, callback) {
+            $http.get(baseURL + 'API_GetUserRankAndScore.php?z=' + user + '&y=' + apiKey + '&u=' + user).then(function (response) {
+                callback(response)
+            });
+        };
 
-      console.log('retroachievements - ready');
-      return retroachievements;
+        retroachievements.GetUserRecentlyPlayedGames = function (user, apiKey, count, offset, callback) {
+            $http.get(baseURL + 'API_GetUserRecentlyPlayedGames.php?z=' + user + '&y=' + apiKey + '&c=' + count + '&o=' + offset + '&u=' + user).then(function (response) {
+                callback(response)
+            });
+        };
+
+        retroachievements.GetFeedFor = function (user, apiKey, count, offset, callback) {
+            $http.get(baseURL + 'API_GetFeedFor.php?z=' + user + '&y=' + apiKey + '&c=' + count + '&o=' + offset + '&u=' + user).then(function (response) {
+                callback(response)
+            });
+        };
+
+        retroachievements.GetUserProgress = function (user, apiKey, gameList, callback) {
+            $http.get(baseURL + 'API_GetUserProgress.php?z=' + user + '&y=' + apiKey + '&u=' + user + "&i=" + gameList).then(function (response) {
+                callback(response)
+            });
+        };
+
+        console.log('retroachievements - ready');
+        return retroachievements;
     }
 ]);
